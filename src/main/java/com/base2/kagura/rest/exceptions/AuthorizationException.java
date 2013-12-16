670c185d849ae0bf52828d5417421a6fa0c165cd
arran ubels <a.ubels@base2services.com>
2013-12-16 15:35:05 +1100

    Basic war created working. Minor refactor.

---
+++ src/main/java/com/base2/kagura/rest/exceptions/AuthorizationException.java
@@ -0,0 +1,11 @@
+package com.base2.kagura.rest.exceptions;
+
+/**
+ * @author aubels
+ *         Date: 30/08/13
+ */
+public class AuthorizationException extends Exception {
+    public AuthorizationException(String s) {
+        super(s);
+    }
+}
