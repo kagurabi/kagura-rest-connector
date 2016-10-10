adffabb1bbcb9b84922803b447a4e3fd582f12e1
arran ubels <a.ubels@base2services.com>
2016-10-10 11:02:35 +1100

    Authentication not required.

---
+++ src/main/java/com/base2/kagura/contribute/dynamodb/authentication/DynamoDBAuthentication.java
@@ -0,0 +1,32 @@
+package com.base2.kagura.contribute.dynamodb.authentication;
+
+/**
+ * Created by arran on 7/10/2016.
+ */
+// TODO. Not required.
+public class DynamoDBAuthentication /*extends AuthenticationProvider*/ {
+//	AWSCredentials credentials;
+//	String usersTable;
+//	String groupsTable;
+//
+//	public DynamoDBAuthentication(AWSCredentials credentials, String usersTable, String groupsTable) {
+//		this.credentials = credentials;
+//		this.usersTable = usersTable;
+//		this.groupsTable = groupsTable;
+//	}
+//
+//	@Override
+//	public List<Group> getGroups() {
+//		return null;
+//	}
+//
+//	@Override
+//	public List<User> getUsers() {
+//		return null;
+//	}
+//
+//	@Override
+//	public void authenticateUser(String user, String pass) throws Exception {
+//
+//	}
+}
